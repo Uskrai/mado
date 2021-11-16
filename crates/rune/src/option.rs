960bf467a948dbd0fc330bf5c747a88723b454fa
Muhammad Fathurrohim <30656271+Uskrai@users.noreply.github.com>
2021-11-16 11:36:50 +0800

    Add Option::flatten

--- crates/rune/src/option.rs
+++ crates/rune/src/option.rs
@@ -21,6 +21,7 @@ pub fn load_module() -> Result<Module, ContextError> {
   let mut module = Module::with_crate_item("std", &["result"]);
   module.inst_fn("ok_or_else", ok_or_else)?;
   module.inst_fn("filter", filter)?;
+  module.inst_fn("flatten", flatten)?;
 
   Ok(module)
 }
@@ -49,5 +50,15 @@ fn filter(
       }
     }
     _ => None,
+  })
+}
+
+fn flatten(option: Option<Value>) -> Result<Option<Value>, VmError> {
+  Ok(match option {
+    Some(v) => match v {
+      Value::Option(v) => flatten(v.take()?)?,
+      val => Some(val),
+    },
+    None => None,
   })
 }
