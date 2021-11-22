eb887dca4223eb0466dac7149eeb31cb35741b58
Muhammad Fathurrohim <30656271+Uskrai@users.noreply.github.com>
2021-11-22 21:43:57 +0800

    Add simple Error Handling

--- crates/relm/src/main.rs
+++ crates/relm/src/main.rs
@@ -22,18 +22,33 @@ use std::sync::Arc;
 #[tokio::main]
 pub async fn main() {
   let mut modules = mado_rune::WebsiteModuleMap::default();
+
+  let load_module = |path: &std::path::Path| -> Result<
+    Vec<mado_rune::WebsiteModule>,
+    Box<dyn std::error::Error>,
+  > {
+    mado_rune::Build::default()
+      .with_path(path)?
+      .build_for_module()?
+      .error_missing_load_module(false)
+      .build()
+      .map_err(Into::into)
+  };
   for it in std::fs::read_dir("../rune/script").unwrap() {
     let it = it.unwrap();
     if it.path().is_file() {
-      let vec = mado_rune::Build::default()
-        .with_path(&it.path())
-        .unwrap()
-        .build_for_module()
-        .unwrap()
-        .build()
-        .unwrap();
-      for it in vec {
-        modules.insert(it);
+      let vec = load_module(&it.path());
+
+      match vec {
+        Ok(vec) => {
+          for it in vec {
+            modules.insert(it);
+          }
+        }
+
+        Err(err) => {
+          println!("{}", err);
+        }
       }
     }
   }
