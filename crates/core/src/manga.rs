d31c5587f134755d7f6236e5eb2702c17ab0a4fd
Muhammad Fathurrohim <30656271+Uskrai@users.noreply.github.com>
2021-11-25 19:15:23 +0800

    Fix still adding ':' when no chapter

--- crates/core/src/manga.rs
+++ crates/core/src/manga.rs
@@ -56,8 +56,11 @@ impl Display for ChapterInfo {
     }
 
     write_if!(volume, "Vol. {} ");
-    write_if!(chapter, "Chapter {}");
-    write_if!(title, ": {}");
+    write_if!(chapter, "Chapter {} ");
+    if self.volume.is_some() || self.chapter.is_some() {
+      write!(f, ": ")?;
+    }
+    write_if!(title, "{}");
     write_if!(scanlator, " [{}]");
     write!(f, " [{}]", self.language)?;
 
