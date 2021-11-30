0886317d539f8bb0ab6ff1b58e6d77f70815ad20
Muhammad Fathurrohim <m.faturrohim13@gmail.com>
2021-11-30 10:23:54 +0800

    Remove mado_relm::MutexWebsiteModuleMap

--- crates/relm/src/app/mod.rs
+++ crates/relm/src/app/mod.rs
@@ -1,7 +1,3 @@
-use std::sync::Mutex;
-
-use mado_core::{ArcWebsiteModule, WebsiteModuleMap};
-
 mod model;
 pub use model::*;
 
@@ -10,29 +6,3 @@ pub use components::*;
 
 mod widgets;
 pub use widgets::*;
-
-pub struct MutexWebsiteModuleMap<Map: WebsiteModuleMap> {
-    map: Mutex<Map>,
-}
-
-impl<Map: WebsiteModuleMap> MutexWebsiteModuleMap<Map> {
-    pub fn new(map: Map) -> Self {
-        Self {
-            map: Mutex::new(map),
-        }
-    }
-}
-
-impl<Map: WebsiteModuleMap> WebsiteModuleMap for MutexWebsiteModuleMap<Map> {
-    fn get_by_uuid(&self, uuid: mado_core::Uuid) -> Option<ArcWebsiteModule> {
-        self.map.lock().unwrap().get_by_uuid(uuid)
-    }
-
-    fn get_by_url(&self, url: mado_core::url::Url) -> Option<ArcWebsiteModule> {
-        self.map.lock().unwrap().get_by_url(url)
-    }
-
-    fn push(&mut self, module: ArcWebsiteModule) -> Result<(), mado_core::WebsiteModuleMapError> {
-        self.map.lock().unwrap().push(module)
-    }
-}
