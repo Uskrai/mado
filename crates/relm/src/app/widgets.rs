1d9998cb532300ee475c5ee86c0395e2594c2339
Muhammad Fathurrohim <m.faturrohim13@gmail.com>
2021-12-29 13:56:03 +0800

    Fix widgets.rs formatting

--- crates/relm/src/app/widgets.rs
+++ crates/relm/src/app/widgets.rs
@@ -6,30 +6,30 @@ use super::AppModel;
 #[relm4_macros::widget(pub)]
 impl Widgets<AppModel, ()> for AppWidgets {
     view! {
-      gtk::ApplicationWindow {
-        set_title: Some("Mado"),
-        set_child = Some(&gtk::Box) {
-          set_orientation: gtk::Orientation::Vertical,
+        gtk::ApplicationWindow {
+            set_title: Some("Mado"),
+            set_child = Some(&gtk::Box) {
+                set_orientation: gtk::Orientation::Vertical,
 
-          append = &gtk::StackSwitcher {
-            set_stack: Some(&stack)
-          },
+                append = &gtk::StackSwitcher {
+                    set_stack: Some(&stack)
+                },
 
-          append: stack = &gtk::Stack {
-            // Download tab
-            add_titled(Some("Download"), "Download") = &gtk::Box {
-              set_orientation: gtk::Orientation::Vertical,
-              append: components.download.root_widget()
-            },
-            // Manga Info tab
-            add_titled(Some("Manga Info"), "Manga Info") = &gtk::Box {
-              set_orientation: gtk::Orientation::Vertical,
-              append: components.manga_info.root_widget()
-            },
-            set_visible_child_name: "Download",
-          },
+                append: stack = &gtk::Stack {
+                    // Download tab
+                    add_titled(Some("Download"), "Download") = &gtk::Box {
+                        set_orientation: gtk::Orientation::Vertical,
+                        append: components.download.root_widget()
+                    },
+                    // Manga Info tab
+                    add_titled(Some("Manga Info"), "Manga Info") = &gtk::Box {
+                        set_orientation: gtk::Orientation::Vertical,
+                        append: components.manga_info.root_widget()
+                    },
+                    set_visible_child_name: "Download",
+                },
 
+            }
         }
-      }
     }
 }
--- crates/relm/src/manga_info/widgets.rs
+++ crates/relm/src/manga_info/widgets.rs
@@ -5,57 +5,57 @@ use relm4::{send, Model, Widgets};
 #[relm4_macros::widget(pub)]
 impl<T: Model> Widgets<MangaInfoModel, T> for MangaInfoWidgets {
     view! {
-      gtk::Box {
-        set_orientation: gtk::Orientation::Vertical,
-        append = &gtk::Box {
-          set_orientation: gtk::Orientation::Horizontal,
-          append : url_entry = &gtk::Entry {
-            // make the entry fill width
-            set_hexpand: true,
-            set_placeholder_text: Some("Enter Manga URL here"),
-            // when user press enter, emit activate to enter button
-            // using emit_activate instead of emit_clicked because
-            // it doesn't animate the "press"
-            connect_activate(enter_button) => move |_| {
-              enter_button.emit_activate();
-            }
-          },
-          // enter button
-          append : enter_button = &gtk::Button {
-            set_label: "⏎",
-            connect_clicked(sender,url_entry) => move |_| {
-              send!(sender, Msg::GetInfo(url_entry.text().to_string()))
-            }
-          }
-        },
+        gtk::Box {
+            set_orientation: gtk::Orientation::Vertical,
+            append = &gtk::Box {
+                set_orientation: gtk::Orientation::Horizontal,
+                append : url_entry = &gtk::Entry {
+                    // make the entry fill width
+                    set_hexpand: true,
+                    set_placeholder_text: Some("Enter Manga URL here"),
+                    // when user press enter, emit activate to enter button
+                    // using emit_activate instead of emit_clicked because
+                    // it doesn't animate the "press"
+                    connect_activate(enter_button) => move |_| {
+                        enter_button.emit_activate();
+                    }
+                },
+                // enter button
+                append : enter_button = &gtk::Button {
+                    set_label: "⏎",
+                    connect_clicked(sender,url_entry) => move |_| {
+                        send!(sender, Msg::GetInfo(url_entry.text().to_string()))
+                    }
+                }
+            },
 
-        append = &gtk::Box {
-            set_vexpand: true,
-            set_hexpand: true,
-            append: components.chapters.root_widget(),
-        },
+            append = &gtk::Box {
+                set_vexpand: true,
+                set_hexpand: true,
+                append: components.chapters.root_widget(),
+            },
 
-        append = &gtk::Box {
-          set_orientation: gtk::Orientation::Horizontal,
+            append = &gtk::Box {
+                set_orientation: gtk::Orientation::Horizontal,
 
-          append: download_path = &gtk::Entry {
-            set_hexpand: true,
-            set_placeholder_text: Some("Enter Download Path"),
-            connect_changed(sender) => move |path| {
-              send!(
-                sender,
-                Msg::DownloadPathChanged(path.text().to_string())
-              );
+                append: download_path = &gtk::Entry {
+                    set_hexpand: true,
+                    set_placeholder_text: Some("Enter Download Path"),
+                    connect_changed(sender) => move |path| {
+                        send!(
+                            sender,
+                            Msg::DownloadPathChanged(path.text().to_string())
+                        );
+                    }
+                },
+
+                append: download_button = &gtk::Button {
+                    set_label: "Download",
+                    connect_clicked(sender) => move |_| {
+                        send!(sender, Msg::Download);
+                    }
+                }
             }
-          },
-
-          append: download_button = &gtk::Button {
-            set_label: "Download",
-            connect_clicked(sender) => move |_| {
-              send!(sender, Msg::Download);
-            }
-          }
         }
-      }
     }
 }
