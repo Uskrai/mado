7c5812b8feb7784a7a5f2d8b7187d280ea3c2928
Muhammad Fathurrohim <m.faturrohim13@gmail.com>
2022-07-12 14:49:36 +0800

    implement From<crate::http> for Client,BodyStream and RequestBuilder

--- crates/core/src/client.rs
+++ crates/core/src/client.rs
@@ -10,3 +10,21 @@ pub enum BodyStream {
 pub enum RequestBuilder {
     Http(crate::http::RequestBuilder),
 }
+
+impl From<crate::http::Client> for Client {
+    fn from(v: crate::http::Client) -> Self {
+        Self::Http(v)
+    }
+}
+
+impl From<crate::http::RequestBuilder> for RequestBuilder {
+    fn from(v: crate::http::RequestBuilder) -> Self {
+        Self::Http(v)
+    }
+}
+
+impl From<crate::http::ResponseStream> for BodyStream {
+    fn from(v: crate::http::ResponseStream) -> Self {
+        Self::Http(v)
+    }
+}
