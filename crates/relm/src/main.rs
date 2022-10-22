d33b3e766ec9423e401a532d5bdb0d13b0396258
Muhammad Fathurrohim <m.faturrohim13@gmail.com>
2022-10-22 17:47:50 +0800

    feat(relm): use deno instead of rune

--- crates/relm/src/main.rs
+++ crates/relm/src/main.rs
@@ -1,4 +1,5 @@
 use anyhow::Context;
+use futures::{SinkExt, StreamExt};
 use mado::core::{ArcMadoModule, DefaultMadoModuleMap, MutexMadoModuleMap};
 use mado::engine::{
     path::Utf8PathBuf, MadoEngine, MadoEngineState, MadoModuleLoader, ModuleLoadError,
@@ -8,11 +9,17 @@ use tracing_subscriber::{util::SubscriberInitExt, EnvFilter};
 
 use std::sync::Arc;
 
-pub struct Loader;
+pub enum LoaderMsg {
+    Load(
+        Utf8PathBuf,
+        futures::channel::oneshot::Sender<Result<Vec<ArcMadoModule>, ModuleLoadError>>,
+    ),
+}
+pub struct Loader(futures::channel::mpsc::Sender<LoaderMsg>);
 #[async_trait::async_trait]
 impl MadoModuleLoader for Loader {
     async fn get_paths(&self) -> Vec<Utf8PathBuf> {
-        let mut dir = tokio::fs::read_dir("../rune/script").await.unwrap();
+        let mut dir = tokio::fs::read_dir("../deno/dist/module").await.unwrap();
 
         let mut paths = Vec::new();
         loop {
@@ -44,30 +51,42 @@ impl MadoModuleLoader for Loader {
         &self,
         path: Utf8PathBuf,
     ) -> Result<Vec<mado::core::ArcMadoModule>, ModuleLoadError> {
-        let result = tokio::task::spawn_blocking(move || load_module(&path))
-            .await
-            .unwrap();
+        let (tx, rx) = futures::channel::oneshot::channel();
 
-        result.map_err(Into::into)
+        self.0
+            .clone()
+            .send(LoaderMsg::Load(path, tx))
+            .await
+            .map_err(anyhow::Error::from)?;
+
+        rx.await.map_err(anyhow::Error::from)?
     }
 }
 
-pub fn load_module(path: &Utf8PathBuf) -> Result<Vec<ArcMadoModule>, ModuleLoadError> {
-    let build = mado_rune::Build::default().with_path(path.as_std_path())?;
+async fn handle_loader_msg(loader: &mut mado_deno::ModuleLoader, msg: LoaderMsg) {
+    match msg {
+        LoaderMsg::Load(path, rx) => {
+            let fun = async {
+                let num = loader.load_file(path.as_std_path()).await?;
 
-    let vec = build
-        .build_for_module()
-        .with_context(|| format!("Error builiding {}", path))?
-        .error_missing_load_module(false)
-        .build()
-        .map_err(anyhow::Error::from)?;
+                let mut vec = Vec::new();
 
-    let mut result = Vec::<ArcMadoModule>::with_capacity(vec.len());
-    for it in vec {
-        result.push(Arc::new(it));
+                let object = loader.init_module(num).await.map_err(anyhow::Error::from)?;
+
+                for (sender, looper) in object.into_iter().flatten() {
+                    tokio::task::spawn_local(looper.start());
+
+                    vec.push(Arc::new(sender) as ArcMadoModule);
+                }
+
+                Ok(vec)
+            };
+
+            let result = fun.await;
+
+            rx.send(result);
+        }
     }
-
-    Ok(result)
 }
 
 pub fn main() {
@@ -94,7 +113,30 @@ pub fn main() {
     let mado = MadoEngine::new(state);
     let model = mado_relm::AppModel::new(mado.state());
 
-    tokio::spawn(mado.load_module(Loader));
+    let (loader_tx, mut loader_rx) = futures::channel::mpsc::channel(5);
+    let deno_loader = Loader(loader_tx);
+
+    let handle = runtime.handle().clone();
+
+    std::thread::Builder::new()
+        .name("deno-runtime".to_string())
+        .spawn(move || {
+            let handle = handle;
+            let task = tokio::task::LocalSet::new();
+            let deno_runtime = mado_deno::Runtime::default();
+            let mut deno_loader = mado_deno::ModuleLoader::from_runtime(deno_runtime);
+
+            task.spawn_local(async move {
+                while let Some(msg) = loader_rx.next().await {
+                    handle_loader_msg(&mut deno_loader, msg).await;
+                }
+            });
+
+            handle.block_on(task);
+        })
+        .unwrap();
+
+    tokio::spawn(mado.load_module(deno_loader));
     tokio::spawn(mado.run());
     tokio::spawn(async {
         let mut channel = channel;
