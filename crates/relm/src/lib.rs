6014865f8b088ca2c4a312d2913213a14533894c
Muhammad Fathurrohim <m.faturrohim13@gmail.com>
2022-10-29 12:05:58 +0800

    feat(relm): remove unused

--- crates/relm/src/lib.rs
+++ crates/relm/src/lib.rs
@@ -8,16 +8,6 @@ pub mod vec_chapters;
 
 pub use app::*;
 
-pub fn get_toplevel(mut widget: gtk::Widget) -> gtk::Window {
-    use gtk::prelude::*;
-
-    while let Some(parent) = widget.parent() {
-        widget = parent;
-    }
-
-    widget.downcast::<gtk::Window>().unwrap()
-}
-
 // macros
 mod dynamic_function;
 mod gobject;
