8662375494b6afe5ff873c6614d10059ae1c4aaa
Muhammad Fathurrohim <m.faturrohim13@gmail.com>
2022-12-26 23:21:11 +0800

    feat(engine): stream debouncer

--- crates/engine/src/info/mod.rs
+++ crates/engine/src/info/mod.rs
@@ -2,12 +2,12 @@ mod chapter;
 mod download;
 mod image;
 mod module;
-mod status;
 mod option;
+mod status;
 
 pub use chapter::{DownloadChapterInfo, DownloadChapterInfoMsg};
 pub use download::{DownloadInfo, DownloadInfoMsg, DownloadRequest, DownloadRequestStatus};
 pub use image::{DownloadChapterImageInfo, DownloadChapterImageInfoMsg};
 pub use module::{LateBindingModule, ModuleInfo, LATE_BINDING_MODULE_SLEEP_TIME};
-pub use status::{DownloadProgressStatus, DownloadResumedStatus, DownloadStatus};
 pub use option::DownloadOption;
+pub use status::{DownloadProgressStatus, DownloadResumedStatus, DownloadStatus};
--- crates/engine/src/timer.rs
+++ crates/engine/src/timer.rs
@@ -5,6 +5,7 @@ use std::{
 };
 
 pub use async_io::Timer;
+use futures::{FutureExt, Stream, StreamExt};
 
 #[derive(Debug, thiserror::Error)]
 #[error("timeout reached")]
@@ -19,8 +20,8 @@ where
     Timeout { timer, future: f }
 }
 
-pub async fn sleep(duration: Duration) {
-    Timer::after(duration).await;
+pub fn sleep(duration: Duration) -> Timer {
+    Timer::after(duration)
 }
 
 #[pin_project::pin_project]
@@ -56,8 +57,96 @@ where
     }
 }
 
+pub struct Delayed<T> {
+    timer: Pin<Box<Timer>>,
+    value: Option<T>,
+}
+
+impl<T> Unpin for Delayed<T> {}
+
+impl<T> Delayed<T> {
+    pub fn new(value: T, delay: Duration) -> Self {
+        Delayed {
+            value: Some(value),
+            timer: Box::pin(sleep(delay)),
+        }
+    }
+}
+
+impl<T> std::future::Future for Delayed<T> {
+    type Output = T;
+
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        match self.timer.poll_unpin(cx) {
+            Poll::Pending => Poll::Pending,
+            Poll::Ready(_) => Poll::Ready(self.value.take().unwrap()),
+        }
+    }
+}
+
+pub struct Debouce<S>
+where
+    S: Stream,
+{
+    stream: S,
+    duration: Duration,
+    pending: Option<Delayed<S::Item>>,
+}
+
+impl<S> Stream for Debouce<S>
+where
+    S: Stream + Unpin,
+{
+    type Item = S::Item;
+
+    fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        while let Poll::Ready(it) = self.stream.poll_next_unpin(cx) {
+            match it {
+                Some(it) => self.pending = Some(Delayed::new(it, self.duration)),
+                None => {
+                    if self.pending.is_none() {
+                        return Poll::Ready(None);
+                    }
+                    break;
+                }
+            }
+        }
+
+        match self.pending.as_mut() {
+            Some(pending) => match pending.poll_unpin(cx) {
+                Poll::Ready(value) => {
+                    let _ = self.pending.take();
+                    Poll::Ready(Some(value))
+                }
+                Poll::Pending => Poll::Pending,
+            },
+            None => Poll::Pending,
+        }
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        self.stream.size_hint()
+    }
+}
+
+pub fn debounce<S>(stream: S, time: Duration) -> Debouce<S>
+where
+    S: Stream + Unpin,
+{
+    Debouce {
+        stream,
+        duration: time,
+        pending: None,
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use std::{sync::Arc, time::Instant};
+
+    use futures::SinkExt;
+    use parking_lot::Mutex;
+
     use super::*;
 
     #[test]
@@ -79,6 +168,85 @@ mod tests {
             )
             .await
             .unwrap_err();
+        });
+    }
+
+    #[test]
+    fn test_delay() {
+        let start = Instant::now();
+        let delayed = futures::executor::block_on(Delayed::new(42, Duration::from_secs(1)));
+        assert_eq!(start.elapsed().as_secs(), 1);
+        assert_eq!(delayed, 42);
+    }
+
+    #[test]
+    fn test_debounce() {
+        futures::executor::block_on(async {
+            let start = Instant::now();
+            let (mut sender, receiver) = futures::channel::mpsc::channel(1024);
+            let mut debounced = debounce(receiver, Duration::from_secs(1));
+            let _ = sender.send(21).await;
+            let _ = sender.send(42).await;
+            assert_eq!(debounced.next().await, Some(42));
+            assert_eq!(start.elapsed().as_secs(), 1);
+            std::mem::drop(sender);
+            assert_eq!(debounced.next().await, None);
+        })
+    }
+
+    #[test]
+    fn test_debounce_order() {
+        #[derive(Debug, PartialEq, Eq)]
+        pub enum Message {
+            Value(u64),
+            SenderEnded,
+            ReceiverEnded,
+        }
+
+        let (mut sender, receiver) = futures::channel::mpsc::channel(1024);
+        let mut receiver = debounce(receiver, Duration::from_millis(100));
+        let messages = Arc::new(Mutex::new(vec![]));
+
+        futures::executor::block_on(async {
+            futures::future::join(
+                {
+                    let messages = messages.clone();
+                    async move {
+                        for i in 0..10u64 {
+                            let _ = sleep(Duration::from_millis(23 * i)).await;
+                            let _ = sender.send(i).await;
+                        }
+
+                        messages.lock().push(Message::SenderEnded);
+                    }
+                },
+                {
+                    let messages = messages.clone();
+
+                    async move {
+                        while let Some(value) = receiver.next().await {
+                            messages.lock().push(Message::Value(value));
+                        }
+
+                        messages.lock().push(Message::ReceiverEnded);
+                    }
+                },
+            )
+            .await;
+
+            assert_eq!(
+                messages.lock().as_slice(),
+                &[
+                    Message::Value(4),
+                    Message::Value(5),
+                    Message::Value(6),
+                    Message::Value(7),
+                    Message::Value(8),
+                    Message::SenderEnded,
+                    Message::Value(9),
+                    Message::ReceiverEnded
+                ]
+            );
         });
     }
 }
